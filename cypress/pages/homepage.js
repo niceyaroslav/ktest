8c708f738e80a1b8189d5d40fbe0f8c40d67d22b
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-10-05 16:39:06 +0300

    trying github actions

--- cypress/pages/homepage.js
+++ cypress/pages/homepage.js
@@ -17,9 +17,6 @@ class homepage {
         this.elements.emailInput().type(email)
         expect(username, 'username was set').to.be.a('string').and.not.be.empty
         this.elements.passwordInput().type(password, {log: false})
-        if (typeof password !== 'string' || !password) {
-            throw new Error('Missing password value, set using CYPRESS_PASSWORD=...')
-          }
         this.elements.loginBtn().click()
     }
 }
