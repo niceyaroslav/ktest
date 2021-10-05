5c57ece499a0e36682edbaedd0a94d3792487fcf
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-10-05 16:53:59 +0300

    still trying

--- cypress/pages/homepage.js
+++ cypress/pages/homepage.js
@@ -15,7 +15,7 @@ class homepage {
     logIn() {
         cy.visit(login)
         this.elements.emailInput().type(email)
-        expect(username, 'username was set').to.be.a('string').and.not.be.empty
+        expect(email, 'email was set').to.be.a('string').and.not.be.empty
         this.elements.passwordInput().type(password, {log: false})
         this.elements.loginBtn().click()
     }
